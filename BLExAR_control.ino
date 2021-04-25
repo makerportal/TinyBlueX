71771921e2fcb27f11ca2f048ecd6a8b7e16171f
Joshua Hrisko <49917891+josh-hrisko@users.noreply.github.com>
2021-04-25 13:16:06 -0400

    Update BLExAR_control.ino

--- BLExAR_control.ino
+++ BLExAR_control.ino
@@ -1,7 +1,7 @@
 /** Control Code for TinyBlueX and BLExAR iOS app
- * -- This code allows the BLExAR iOS app to control the pins on 
- * -- the TinyBlueX combined ATtiny85 microcontroller and Bluetooth 
- * -- Low Energy module (CC254x chip also known as: 
+ * -- This code allows the BLExAR iOS app to control the pins on
+ * -- the TinyBlueX combined ATtiny85 microcontroller and Bluetooth
+ * -- Low Energy module (CC254x chip also known as:
  * -- HM-10, AT-09, MLT-BT05, JDY-08, etc.)
  *  
  *  by Joshua Hrisko, Maker Portal LLC (c) 2021
@@ -11,9 +11,11 @@
 
 SoftwareSerial ble_device(0,1); // BLE TX-> ATtiny85 PB0, BLE RX-> ATtiny85 PB1
 
-int pin_states[5] = {0,0,0,0}; // pin states variable
-String tot_val = ""; // storage variable for serial data
-int start_offset = 2; // starting pin (#2 for ATtiny85 to avoid BLE conflict on 0/1)
+String strglob= "";
+String pin_num_char = "";
+int pin_num = 255;
+bool analog_bool = false;
+bool digital_bool = false;
 
 void setup() {
   ble_device.begin(9600); // start BLE device
@@ -22,32 +24,62 @@ void setup() {
   // Once the code is uploaded once, the below can be commented out
   // (the name change only needs to occur once)
   ble_device.println("AT+NAMETinyBlue"); // change device name
-  delay(100); // wait for change
+  delay(250); // wait for change
   ble_device.println("AT+RESET"); // reset module to enact name change
-  delay(250); // wait for reset
-
-  // Setting pins as outputs
-  for (int ii = start_offset;ii<(sizeof(pin_states)/sizeof(bool))+start_offset;ii++){
-    pinMode(ii,OUTPUT); // set pins as outputs
-  }
+  delay(500); // wait for reset
 }
 
 void loop() {
-  char ser_byte = ble_device.read();
-  if (int(ser_byte)!=-1){ // make sure it's a valid integer pin
-    tot_val+=String(ser_byte); // if the input is over 1 character, this handles that
-    tot_val.trim(); // trim any white space
-    tot_val.replace(".",""); // the . and @ must be stripped from reading
-    tot_val.replace("@",""); // to prevent errors (. and @ should not be used in comm.)
-    if (ser_byte=='\n'){ // wait for newline
-      int indx = String(tot_val).toInt()-start_offset; // index of pin to control
-      if (indx<sizeof(pin_states)/sizeof(bool) && indx>=0){ // only alter open pin values
-        pin_states[indx] = !bool(pin_states[indx]); // array pin state
-        digitalWrite(String(tot_val).toInt(),pin_states[indx]); // turn pin on/off        
+  char ble_char = ble_device.read();
+  if (int(ble_char)!=-1){ // make sure it's a valid character  
+    if (ble_char == '\n'){
+      if (analog_bool){
+        pinMode(pin_num,OUTPUT);
+        delay(50);
+        analogWrite(pin_num,strglob.toInt());
+        analog_bool = false;
+        pin_num = 255;
+      } else if (digital_bool){
+        pinMode(pin_num,OUTPUT);
+        delay(50);
+        if (strglob == "1"){
+          digitalWrite(pin_num,HIGH);
+        } else if (strglob == "0"){
+          digitalWrite(pin_num,LOW);
+          pinMode(pin_num,INPUT);
+          delay(50);
+        }
+        digital_bool = false;
+        pin_num = 255;
       }
-      delay(50); // required delay before TX (after RX) - for stability
-      ble_device.println(tot_val); // uncomment to print reading
-      tot_val = ""; // clear serial input variable
+      pin_num = 255;
+      strglob = "";
+    } else {
+      if (analog_bool == false and digital_bool == false) {
+        if (ble_char == 'D'){
+          digital_bool = true;
+        } else if (ble_char == 'A'){
+          analog_bool = true;
+        } else {
+        }
+      } else{
+        if (pin_num == 255){
+          if (ble_char != 'e'){
+            pin_num_char += ble_char;
+          } else if (ble_char == 'e'){
+            pin_num = (String(pin_num_char).toInt());
+            pin_num_char = "";
+          }
+        } else {
+          if (int(ble_char)!=-1 && ble_char!='.' && ble_char!='@' && ble_char!=' ' && ble_char!='\r' && ble_char!='\n' && ble_char!='\0'){
+            strglob += String(ble_char);    
+          }
+        }
+      }
     }
+  }
+  if ((ble_char)!=-1 && ble_char!=' ' && ble_char!='\n' && ble_char!='\r' && ble_char!='@' && ble_char!='.' && ble_char!='\0'){
+    delay(50);
+//    ble_device.println(ble_char); // uncomment to send back to BLExAR
   }
 }
